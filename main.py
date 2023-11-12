faed58b96ee308505c91d164b2fd85ed6be3aa53
hydrotho <42911474+hydrotho@users.noreply.github.com>
2023-11-12 19:52:20 +0800

    ✨ feat(main.py): add functionality to send a Telegram message with live stream information The main.py file has been added to the project. It contains the following changes: - Added imports for necessary modules and libraries - Defined functions to retrieve live room information and user information from the Bilibili API - Implemented a function to send a Telegram message with live stream information using the python-telegram-bot library - Created a LiveRoom class to track the status of a live room - Implemented a MyHandler class to handle events from the blivedm library and manage multiple live rooms - Added a reminder function to start the live stream reminder process - Modified the main function to read configuration from a YAML file and start the reminder process These changes were made to add the functionality of sending a Telegram message with live stream information to notify users about ongoing live streams on Bilibili.

---
+++ main.py
@@ -0,0 +1,147 @@
+import asyncio
+import logging
+import os
+import sys
+
+import requests
+import typer
+import yaml
+from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup
+
+import blivedm.blivedm as blivedm
+
+logger = logging.getLogger("BiliBili_Livestream_Reminder")
+
+bot_token = ""
+chat_id = ""
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
+}
+
+
+def get_live_room_info(room_id):
+    url = f"https://api.live.bilibili.com/room/v1/Room/get_info?room_id={room_id}"
+    response = requests.get(url, headers=HEADERS)
+    if response.status_code != 200:
+        logger.error("获取直播间信息失败: HTTP 状态码 %s", response.status_code)
+        return None
+    data = response.json()
+    if data["code"] != 0:
+        logger.error("获取直播间信息失败: %s", data["message"])
+        return None
+    return data["data"]
+
+
+def get_user_info(uid):
+    url = f"https://api.live.bilibili.com/live_user/v1/Master/info?uid={uid}"
+    response = requests.get(url, headers=HEADERS)
+    if response.status_code != 200:
+        logger.error("获取用户信息失败: HTTP 状态码 %s", response.status_code)
+        return None
+    data = response.json()
+    if data["code"] != 0:
+        logger.error("获取用户信息失败: %s", data["message"])
+        return None
+    return data["data"]
+
+
+async def send_telegram_message(live_room_info, user_info):
+    bot = Bot(bot_token)
+    caption = f"[{user_info['info']['uname']}](https://space.bilibili.com/{user_info['info']['uid']}) 正在直播\n标题：{live_room_info['title']}"
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                "观看直播", url=f"https://live.bilibili.com/{live_room_info['room_id']}"
+            )
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await bot.send_photo(
+        chat_id=chat_id,
+        photo=live_room_info["user_cover"],
+        caption=caption,
+        reply_markup=reply_markup,
+        parse_mode="Markdown",
+    )
+
+
+class LiveRoom:
+    def __init__(self, room_id):
+        self.room_id = room_id
+        self.is_live = False
+
+    def on_preparing(self):
+        self.is_live = False
+
+    async def on_live(self):
+        if self.is_live:
+            return
+        self.is_live = True
+        live_room_info = get_live_room_info(self.room_id)
+        if live_room_info is None:
+            return
+        user_info = get_user_info(live_room_info["uid"])
+        if user_info is None:
+            return
+        await send_telegram_message(live_room_info, user_info)
+
+
+class MyHandler(blivedm.BaseHandler):
+    def __init__(self):
+        self.rooms = {}
+
+    def add_room(self, room_id):
+        self.rooms[room_id] = LiveRoom(room_id)
+
+    def _on_preparing(self, client: blivedm.BLiveClient, _):
+        room = self.rooms.get(client.room_id)
+        if room:
+            room.on_preparing()
+
+    def _on_live(self, client: blivedm.BLiveClient, _):
+        room = self.rooms.get(client.room_id)
+        if room:
+            asyncio.create_task(room.on_live())
+
+    _CMD_CALLBACK_DICT = blivedm.BaseHandler._CMD_CALLBACK_DICT.copy()
+    _CMD_CALLBACK_DICT["PREPARING"] = _on_preparing
+    _CMD_CALLBACK_DICT["LIVE"] = _on_live
+
+
+async def reminder(room_ids):
+    handler = MyHandler()
+    for room_id in room_ids:
+        handler.add_room(room_id)
+
+    clients = [blivedm.BLiveClient(room_id) for room_id in room_ids]
+    for client in clients:
+        client.set_handler(handler)
+        client.start()
+
+    try:
+        await asyncio.gather(*(client.join() for client in clients))
+    finally:
+        await asyncio.gather(*(client.stop_and_close() for client in clients))
+
+
+def main(
+    config: str = "config.yaml",
+):
+    global bot_token, chat_id
+
+    if not os.path.isabs(config):
+        base_path = os.path.dirname(sys.argv[0])
+        config = os.path.join(base_path, config)
+
+    with open(config, "r") as file:
+        c = yaml.safe_load(file)
+    bot_token = c["telegram_bot_token"]
+    chat_id = c["telegram_chat_id"]
+    room_ids = c["room_ids"]
+
+    asyncio.run(reminder(room_ids))
+
+
+if __name__ == "__main__":
+    typer.run(main)
